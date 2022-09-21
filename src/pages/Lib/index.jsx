b30c0de98adf91ee84c211d1650ed642d306b0fa
Yuri Bragine <iubragine@gmail.com>
2022-09-20 21:51:17 -0300

    Cria o componente header da lib

--- src/pages/Lib/index.jsx
+++ src/pages/Lib/index.jsx
@@ -1,3 +1,16 @@
+import React from 'react';
+import './style.css';
+// import { Field } from "formik";
+
 export function Lib(){
-    return <h1>Crie aqui sua própria playlist</h1>
+    return (
+        
+
+    <div className='header'>
+        <img src='./Images/logo-pequena.png' alt='logo principal' height='60px'></img>
+        <input className='searchBar' type="text" placeholder= "O que você quer ouvir?"/>
+        {/* <Field type= "text" name= "search" placeholder= "O que você quer ouvir" /> */}
+    </div>
+   
+    )
 }
