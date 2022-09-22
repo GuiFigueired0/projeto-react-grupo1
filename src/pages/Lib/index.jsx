8b5d63707143a7a6bd12cf8fa0939f8f58038a04
Yuri Bragine <iubragine@gmail.com>
2022-09-21 22:43:34 -0300

    Cria o container de playlists

--- src/pages/Lib/index.jsx
+++ src/pages/Lib/index.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
-import './style.css';
-// import { Field } from "formik";
+import './styleLib.css';
 
 export function Lib(){
     return (
         
-
-    <div className='header'>
-        <img src='./Images/logo-pequena.png' alt='logo principal' height='60px'></img>
-        <input className='searchBar' type="text" placeholder= "O que você quer ouvir?"/>
-        {/* <Field type= "text" name= "search" placeholder= "O que você quer ouvir" /> */}
-    </div>
-   
+    <>
+        <header className='mainHeader'>
+            <img src='./Images/logo-pequena.png' alt='logo principal' height='60px'></img>
+            <input className='searchBar' type="text" placeholder= "O que você quer ouvir?"/>
+        </header>
+        <div className='libraryContent'>
+            <h1 className='mainTitle'>Sua Biblioteca:</h1>
+            <div className='playlist'>
+                <img className='firstPlaylist' src="./Images/disc.jpeg" alt="Imagem de um disco de vinil" />
+                <h2 className='playlistTitle'>Nome da Musica</h2>
+                <a className='music' href="">Musica 1...</a>
+                <a className='music' href="">Musica 2...</a>
+                <a className='music' href="">Musica 3...</a>
+            </div>
+            <div className='playlist'>
+                <img className='firstPlaylist' src="./Images/disc.jpeg" alt="Imagem de um disco de vinil" />
+                <h2 className='playlistTitle'>Nome da Musica</h2>
+                <a className='music' href="">Musica 1...</a>
+                <a className='music' href="">Musica 2...</a>
+                <a className='music' href="">Musica 3...</a>
+            </div>
+            <div className='playlist'>
+                <img className='firstPlaylist' src="./Images/disc.jpeg" alt="Imagem de um disco de vinil" />
+                <h2 className='playlistTitle'>Nome da Musica</h2>
+                <a className='music' href="">Musica 1...</a>
+                <a className='music' href="">Musica 2...</a>
+                <a className='music' href="">Musica 3...</a>
+            </div>
+        </div>
+    </>
     )
 }
